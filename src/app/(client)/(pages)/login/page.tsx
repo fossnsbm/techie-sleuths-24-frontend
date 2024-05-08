18ce547d59ee306f42d3caa250bd9050ba40a15b
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-05-08 19:56:47 +0530

    feat: Login Page

---
+++ src/app/(client)/(pages)/login/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+import Container from "@/components/layout/Container";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="h-[105vh] sm:h-[115vh] md:h-[120vh] relative">
+      <Image
+        alt="Mountains"
+        src="/assets/bg-login-img.png"
+        objectFit="cover"
+        layout="fill"
+      />
+
+      <Container>
+        <div className="w-full h-screen pt-[0%] flex justify-center items-center">
+          <div className="border-4 border-[#A66224] w-[90%] sm:w-[80%] md:w-[50%] lg:w-[40%] xl:w[30%] 2xl:w-[30%] h-auto p-[10px_0px_50px_0px] drop-shadow-lg shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] bg-[#FEF3C7] flex flex-col items-center">
+            <div className="w-[10px] h-[10px] bg-[#A66224] absolute left-0 top-0 ml-1 mt-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] "></div>
+            <div className="w-[10px] h-[10px] bg-[#A66224] absolute right-0 top-0 mr-1 mt-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]"></div>
+            <div className="w-[10px] h-[10px] bg-[#A66224] absolute bottom-0 left-0 mb-1 ml-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]"></div>
+            <div className="w-[10px] h-[10px] bg-[#A66224] absolute bottom-0 right-0 mb-1 mr-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]"></div>
+            <h1
+              className="text-5xl text-center text-[#FEF3C7]"
+              style={{
+                WebkitTextStroke: "2px #5A270B",
+                textShadow:
+                  "4px 4px 0 #78350f, -4px -4px 0 #FEF3C7, 4px -4px 0 #FEF3C7, -4px 4px 0 #FEF3C7",
+              }}
+            >
+              LET'S GO
+            </h1>
+            <Input
+              className=" transition duration-300 text-[20px] placeholder:text-[#5A270B] text-[#5A270B] mt-5 bg-[#FEF3C7] w-[80%] sm:w-[70%] h-[12%] border-4 border-[#A66224]  drop-shadow-lg hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,0)]  shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] rounded-none "
+              type="email"
+              placeholder="TEAM EMAIL*"
+            />
+            <Input
+              className="transition duration-300 text-[20px] placeholder:text-[#5A270B]  text-[#5A270B] mt-5 bg-[#FEF3C7] w-[80%]  sm:w-[70%] h-[12%] border-4 border-[#A66224]  drop-shadow-lg hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,0)] shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] rounded-none "
+              type="email"
+              placeholder="PASSWORD*"
+            />
+
+            <div className="flex text-[#5A270B] my-9 px-3">
+              <center>
+                <h1>
+                  Forget password?{" "}
+                  <Link
+                    href="/"
+                    className="underline cursor-pointer hover:text-[#A66224] transition duration-200 active:text-[#A66224] "
+                  >
+                    Get a new one
+                  </Link>
+                </h1>
+              </center>
+            </div>
+            <Button
+              className="mt-5"
+              onClick={() => console.log("Hello")}
+              loading={false}
+            >
+              PLAY
+            </Button>
+          </div>
+        </div>
+      </Container>
+      <div className="w-[100%]  h-auto absolute bottom-0">
+        <div className="flex  justify-between">
+          <Image
+            src="/assets/item-1.png"
+            alt="item-1"
+            width={50}
+            height={100}
+            className="w-150  ml-10 left-0"
+          />
+          <div className="flex">
+            <Image
+              src="/assets/axe-2.png"
+              alt="item-1"
+              width={50}
+              height={100}
+              className="w-150 mr-5 sm:mr-20 "
+            />
+
+            <Image
+              src="/assets/bush-1.png"
+              alt="item-1"
+              width={80}
+              height={50}
+              className="w-150 mr-0 hidden sm:block md:mr-20 "
+            />
+          </div>
+        </div>
+        <div className="">
+          <Image
+            src="/assets/login-floor.png"
+            alt="login floor"
+            width={6000}
+            height={100}
+            className=" w-full h-30 object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
