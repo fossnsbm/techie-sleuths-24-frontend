db61fc620d4093b34182f2151ee1291cb7804eeb
Thushan W A R T <rusiththarindu2@gmail.com>
2024-05-09 21:38:53 +0530

    feat: Footer data

---
+++ src/data/footer/footerData.ts
@@ -0,0 +1,34 @@
+const socialMediaData = [
+    {
+        platform: "Facebook",
+        url: "https://www.facebook.com",
+        alt: "Facebook",
+        src: "/assets/logos/fb_logo.png"
+    },
+    {
+        platform: "Instagram",
+        url: "https://www.instagram.com",
+        alt: "Instagram",
+        src: "/assets/logos/insta_logo.png"
+    },
+    {
+        platform: "LinkedIn",
+        url: "https://www.linkedin.com",
+        alt: "LinkedIn",
+        src: "/assets/logos/lin_logo.png"
+    },
+    {
+        platform: "YouTube",
+        url: "https://www.youtube.com",
+        alt: "YouTube",
+        src: "/assets/logos/yt_logo.png"
+    },
+    {
+        platform: "Twitter",
+        url: "https://www.twitter.com",
+        alt: "Twitter",
+        src: "/assets/logos/x_logo.png"
+    }
+];
+
+export default socialMediaData;
