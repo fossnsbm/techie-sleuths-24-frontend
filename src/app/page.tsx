e01c1bddf182f1014c17df26b5c984abf0e7e672
Lithara Perera <litharaperera2002@gmail.com>
2024-05-09 12:23:56 +0530

    feat: Register now section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import ExampleCard from "@/components/cards/ExampleCard";
+import RegisterNowCard from "@/components/cards/registerNowCard";
 import ExampleForm from "@/components/forms/ExampleForm";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center gap-10">
-      <ExampleForm />
-      <ExampleCard />
+    <div>
+      <div className="flex flex-col h-screen justify-center items-center gap-10">
+        <ExampleForm />
+        <ExampleCard />
+      </div>
+      <div className="hidden md:block">
+        <RegisterNowCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/cards/registerNowCard.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import Container from "../layout/Container";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+export default function RegisterNowCard() {
+  return (
+    <div className="bg-slate-900">
+      <Container>
+        <div className="relative flex items-center justify-center py-12">
+          <Image
+            src="/assets/register-now-bg.png"
+            width={1200}
+            height={100}
+            alt="Register Now"
+          />
+          <div className="w-full absolute z-10 flex flex-col gap-8 md:flex-row items-center justify-between lg:px-36 md:px-16">
+            <div className="w-full md:w-1/2 uppercase text-base md:text-3xl lg:text-6xl">
+              lorem ipsum dolor sit amet consectetur?
+            </div>
+            <div className="flex flex-col gap-6 items-center justify-center">
+              <Button size="lg">Coming Soon</Button>
+              <div className="md:text-base font-mono text-center">*The registration will be opening soon</div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
