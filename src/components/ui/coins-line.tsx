5be211d8a8b20da8367361d8cef6f5f9ea5472e4
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-10 11:49:50 +0530

    fix: Coins row and gaps in event details

--- src/components/ui/coins-line.tsx
+++ src/components/ui/coins-line.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React from 'react';
 
 interface CoinsRowProps {
@@ -8,34 +8,39 @@ interface CoinsRowProps {
 
 const CoinsRow: React.FC<CoinsRowProps> = ({ centerCount, sideCount }) => {
     const totalImages = centerCount + 2 * sideCount;
-
     const images = Array(totalImages).fill({ src: "/assets/coin.png", alt: "Coin" });
 
     const centerStartIndex = sideCount;
     const centerEndIndex = centerStartIndex + centerCount;
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%', overflow: 'visible' }}>
             {images.map((image, index) => {
-                let opacity = 0;
+                let opacity = 0.2;
 
                 if (index >= centerStartIndex && index < centerEndIndex) {
                     opacity = 1;
                 } else if (index < centerStartIndex) {
                     const distanceFromCenter = centerStartIndex - index;
-                    opacity = 1 - distanceFromCenter * (1 / sideCount);
+                    opacity = 1 - ((distanceFromCenter - 1) * (1 / sideCount));
+                    opacity = Math.max(opacity, 0.1);
                 } else {
                     const distanceFromCenter = index - centerEndIndex;
-                    opacity = 1 - distanceFromCenter * (1 / sideCount);
+                    opacity = 1 - (distanceFromCenter * (1 / sideCount));
+                    opacity = Math.max(opacity, 0.1);
                 }
 
                 return (
-                <img
-                    key={index}
-                    src={image.src}
-                    alt={image.alt}
-                    style={{ opacity, transition: 'opacity 0.5s' }}
-                />
+                    <img
+                        key={index}
+                        src={image.src}
+                        alt={image.alt}
+                        style={{
+                            opacity,
+                            transition: 'opacity 0.5s',
+                            margin: '0 10px',
+                        }}
+                    />
                 );
             })}
         </div>
