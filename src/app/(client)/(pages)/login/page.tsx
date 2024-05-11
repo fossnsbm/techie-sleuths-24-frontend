e456a16553f3568877805b8c49552023ebfad5ae
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-05-11 14:05:58 +0530

    patch: Login floor image change

--- src/app/(client)/(pages)/login/page.tsx
+++ src/app/(client)/(pages)/login/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
       />
 
       <Container>
-        <div className="w-full h-screen pt-[0%] flex justify-center items-center">
+        <div className="w-full h-screen  flex justify-center items-center relative z-10 ">
           <div className="border-4 border-[#A66224] w-[90%] sm:w-[80%] md:w-[50%] lg:w-[40%] xl:w[30%] 2xl:w-[30%] h-auto p-[10px_0px_50px_0px] drop-shadow-lg shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] bg-[#FEF3C7] flex flex-col items-center">
             <div className="w-[10px] h-[10px] bg-[#A66224] absolute left-0 top-0 ml-1 mt-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] "></div>
             <div className="w-[10px] h-[10px] bg-[#A66224] absolute right-0 top-0 mr-1 mt-1 drop-shadow-lg shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]"></div>
@@ -67,41 +67,36 @@ const page = () => {
         </div>
       </Container>
       <div className="w-[100%]  h-auto absolute bottom-0">
-        <div className="flex  justify-between">
-          <Image
-            src="/assets/item-1.png"
-            alt="item-1"
-            width={50}
-            height={100}
-            className="w-150  ml-10 left-0"
-          />
-          <div className="flex">
+        <Container>
+          <div className="flex  justify-between">
             <Image
-              src="/assets/axe-2.png"
+              src="/assets/item-1.png"
               alt="item-1"
               width={50}
               height={100}
-              className="w-150 mr-5 sm:mr-20 "
+              className="w-150  ml-10 left-0"
             />
+            <div className="flex">
+              <Image
+                src="/assets/axe-2.png"
+                alt="item-1"
+                width={50}
+                height={100}
+                className="w-150 mr-5 sm:mr-20 "
+              />
 
-            <Image
-              src="/assets/bush-1.png"
-              alt="item-1"
-              width={80}
-              height={50}
-              className="w-150 mr-0 hidden sm:block md:mr-20 "
-            />
+              <Image
+                src="/assets/bush-1.png"
+                alt="item-1"
+                width={80}
+                height={50}
+                className="w-150 mr-0 hidden sm:block md:mr-20 "
+              />
+            </div>
           </div>
-        </div>
-        <div className="">
-          <Image
-            src="/assets/login-floor.png"
-            alt="login floor"
-            width={6000}
-            height={100}
-            className=" w-full h-30 object-cover"
-          />
-        </div>
+        </Container>
+
+        <div className="bg-[#A66224] w-full h-[40px] md:h-[100px] lg:h-[130px] xl:h-[170px] bg-[url('/assets/footerAssets/brick-gradient.png')] bg-contain bg-repeat-x bg-center rotate-180 "></div>
       </div>
     </div>
   );
