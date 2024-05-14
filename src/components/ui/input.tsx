9ba64b51df947e7159ef3b76e88ec18e3132548f
Lithara Perera <litharaperera2002@gmail.com>
2024-05-14 17:19:09 +0530

    refactor: Registration form

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "font-mono font-bold flex transition duration-300 text-[20px] placeholder:text-[#5A270B] text-[#5A270B] py-3 pl-4 text-2xl bg-[#FEF3C7] w-[80%] sm:w-[70%] h-[12%] border-[6px] border-[#5A270B]  drop-shadow-lg hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,0)]  shadow-[6px_6px_0px_0px_rgba(90,39,11,1)] rounded-none ",
+          "font-mono font-bold flex transition duration-300 text-[20px] placeholder:text-[#5A270B] text-[#5A270B] py-3 pl-4 text-base md:text-2xl bg-[#FEF3C7] w-[80%] sm:w-[70%] h-[12%] border-[6px] border-[#5A270B]  drop-shadow-lg hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,0)]  shadow-[6px_6px_0px_0px_rgba(90,39,11,1)] rounded-none ",
           className
         )}
         ref={ref}
