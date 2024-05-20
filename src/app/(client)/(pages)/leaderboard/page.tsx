ff16987d9c4704dcb716a3ce11afbbb1899c3dac
Lithara Perera <litharaperera2002@gmail.com>
2024-05-20 21:54:44 +0530

    refactor: Leaderboard

--- src/app/(client)/(pages)/leaderboard/page.tsx
+++ src/app/(client)/(pages)/leaderboard/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import QuestionBox from "./_components/question-box";
 import { BarList, Card } from "@tremor/react";
 
 export default function Page() {
@@ -96,26 +95,6 @@ export default function Page() {
       </div>
       {/* <Card className="mx-auto max-w-lg">
       </Card> */}
-
-      {/* <div className="w-full h-[85vh] relative flex justify-center items-center flex-col gap-8">
-        <Image
-          alt="start"
-          src="/assets/start.png"
-          width={150}
-          height={150}
-          className="animate-bounce hover:animate-spin"
-        />
-        <h1
-          className="text-black text-5xl text-center "
-          style={{
-            textShadow:
-              "2px 2px 0 #fff, -2px -2px 0 #fff, 2px -2px 0 #fff, -2px 2px 0 #fff",
-          }}
-        >
-          The Leaderboard will be available when the event starts!
-        </h1>
-        <QuestionBox />
-      </div> */}
     </div>
   );
 }
