de4e0f5a58f96a38ffb7eabbfce8afa8c12c72eb
Lithara Perera <litharaperera2002@gmail.com>
2024-05-20 13:22:04 +0530

    refactor: Leaderboard barlist background

--- src/app/(client)/(pages)/leaderboard/page.tsx
+++ src/app/(client)/(pages)/leaderboard/page.tsx
@@ -5,30 +5,72 @@ import { BarList, Card } from "@tremor/react";
 export default function Page() {
   const data = [
     {
+      rank: 1,
       name: "Google",
       value: 351,
       href: "https://google.com",
-      
     },
     {
+      rank: 2,
       name: "GitHub",
       value: 271,
       href: "https://github.com/tremorlabs/tremor",
-      
     },
     {
+      rank: 3,
       name: "Reddit",
       value: 191,
       href: "https://reddit.com",
-      
     },
     {
+      rank: 4,
       name: "Youtube",
       value: 91,
       href: "https://www.youtube.com/@tremorlabs3079",
-      
+    },
+    {
+      rank: 5,
+      name: "Google",
+      value: 351,
+      href: "https://google.com",
+    },
+    {
+      rank: 6,
+      name: "GitHub",
+      value: 271,
+      href: "https://github.com/tremorlabs/tremor",
+    },
+    {
+      rank: 7,
+      name: "Reddit",
+      value: 191,
+      href: "https://reddit.com",
+    },
+    {
+      rank: 8,
+      name: "Youtube",
+      value: 91,
+      href: "https://www.youtube.com/@tremorlabs3079",
+    },
+    {
+      rank: 9,
+      name: "Google",
+      value: 351,
+      href: "https://google.com",
+    },
+    {
+      rank: 10,
+      name: "GitHub",
+      value: 271,
+      href: "https://github.com/tremorlabs/tremor",
     },
   ];
+
+  const style = {
+    boxShadowLittle: "3px 3px #000",
+    boxShadowLarge: "8px 8px #000",
+    littleBoxStyle: "absolute aspect-square w-3 bg-[#A66224]",
+  };
 
   return (
     <div className="w-full h-full relative bg-slate-900">
@@ -38,13 +80,22 @@ export default function Page() {
         layout="fill"
         objectFit="cover"
       />
-      <Card className="mx-auto max-w-lg">
+      <div
+        style={{
+          boxShadow: style.boxShadowLarge,
+        }}
+        className="relative mx-7  md:max-w-5xl md:mx-auto bg-[#FEF3C7] py-10 px-10 border-8 border-[#A66224]"
+      >
         <p className="mt-4 text-tremor-default flex items-center justify-between text-tremor-content dark:text-dark-tremor-content">
+          <span>Rank</span>
           <span>Team Name</span>
           <span>Points</span>
         </p>
         <BarList data={data} className="mt-2" />
-      </Card>
+
+      </div>
+      {/* <Card className="mx-auto max-w-lg">
+      </Card> */}
 
       {/* <div className="w-full h-[85vh] relative flex justify-center items-center flex-col gap-8">
         <Image
