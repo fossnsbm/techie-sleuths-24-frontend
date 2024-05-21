9b567337090d4bf8cfe9a6dcb5971aa1bbc63de5
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-05-22 03:47:11 +0530

    refactor: Navbar

--- src/components/layout/nav-hamburger.tsx
+++ src/components/layout/nav-hamburger.tsx
@@ -58,8 +58,8 @@ export default function NavigationMenuHamburger() {
           ))}
         </div>
 
-        <Link href={"/register"}>
-          <Button className="ml-10">REGISTER</Button>
+        <Link href={"/login"}>
+          <Button className="ml-10">LOGIN</Button>
         </Link>
       </div>
     </div>
