e09fa1f617b793fe1b5fb3b8e611353185b1d169
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-21 12:13:19 +0530

    refactor: Data mapping in leaderboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -134,7 +134,7 @@ const config = {
       fontSize: {
         "tremor-label": ["0.75rem", { lineHeight: "1rem" }],
         "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
-        "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
+        "tremor-title": ["1.15rem", { lineHeight: "1.75rem" }],
         "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
       },
       borderRadius: {
